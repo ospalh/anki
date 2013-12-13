ee570002a672c91bcc5c66865f4192299b15024e
ospalh <ospalh@gmail.com>
2013-12-13 10:09:44 +0100

    Use StrictVersion to enforce Python 2.6 or 2.7.

--- anki/__init__.py
+++ anki/__init__.py
@@ -2,15 +2,21 @@
 # Copyright: Damien Elmes <anki@ichi2.net>
 # License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 
+from distutils.version import StrictVersion
 import os
 import platform
 import sys
 
-if sys.version_info[0] > 2:
-    raise Exception("Anki should be run with Python 2")
-elif sys.version_info[1] < 6:
-    raise Exception("Anki requires Python 2.6+")
-elif sys.getfilesystemencoding().lower() in ("ascii", "ansi_x3.4-1968"):
+
+sys_strict_version = StrictVersion('{}.{}.{}'.format(
+        sys.version_info.major,
+        sys.version_info.minor,
+        sys.version_info.micro))
+if sys_strict_version < StrictVersion('2.6'):
+    raise Exception("Please upgrade your Python to 2.6 or 2.7")
+if sys_strict_version >= StrictVersion('3.0'):
+    raise Exception("Anki does not work with Python 3")
+if sys.getfilesystemencoding().lower() in ("ascii", "ansi_x3.4-1968"):
     raise Exception("Anki requires a UTF-8 locale.")
 
 try:
