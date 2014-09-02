e590715b9385022e3300874fca896b9b9843f1d9
ospalh <ospalh@gmail.com>
2014-09-02 16:48:21 +0200

    Put the apkg on top when importing.

--- anki/importing/__init__.py
+++ anki/importing/__init__.py
@@ -12,8 +12,8 @@ from anki.importing.pauker import PaukerImporter
 from anki.lang import _
 
 Importers = (
-    (_("Text separated by tabs or semicolons (*)"), TextImporter),
     (_("Packaged Anki Deck (*.apkg *.zip)"), AnkiPackageImporter),
+    (_("Text separated by tabs or semicolons (*)"), TextImporter),
     (_("Anki 1.2 Deck (*.anki)"), Anki1Importer),
     (_("Mnemosyne 2.0 Deck (*.db)"), MnemosyneImporter),
     (_("Supermemo XML export (*.xml)"), SupermemoXmlImporter),
